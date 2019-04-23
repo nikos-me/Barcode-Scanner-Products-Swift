9841e72e9c43cdb11011ea97855eb9c099383292
Nikos Menexis <men.nikos@gmail.com>
2019-04-23 13:25:49 +0300

    Initial Commit

---
+++ ProductsBarcodeScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductsBarcodeScanner
+//
+//  Created by Nikos Menexis on 23/04/2019.
+//  Copyright © 2019 Nikos Menexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
